b4aecb24ed33e9438904aa185afa3305abe9f61a
cleybson cardoso <cleybsonc@gmail.com>
2017-03-16 21:54:24 -0300

    pega lucas

--- smartOffice/src/providers/geo-service.ts
+++ smartOffice/src/providers/geo-service.ts
@@ -4,7 +4,6 @@ import 'rxjs/add/operator/filter';
 
 /*
   Generated class for the GeoService provider.
-
   See https://angular.io/docs/ts/latest/guide/dependency-injection.html
   for more info on providers and Angular 2 DI.
 */
@@ -38,6 +37,8 @@ export class GeoService {
       this.zone.run(() => {
         this.lat = data.coords.latitude;
         this.lng = data.coords.longitude;
+
+        alert(this.getDistanceFromLatLonInKm(this.lat, this.lng, 0, 0));
       });
       // data can be a set of coordinates, or an error (if an error occurred).
       // data.coords.latitude
@@ -49,6 +50,24 @@ export class GeoService {
   stopTracking() {
 
 
+  }
+
+  private getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = this.deg2rad(lat2 - lat1);  // deg2rad below
+    var dLon = this.deg2rad(lon2 - lon1);
+    var a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2)
+      ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return d;
+  }
+
+  private deg2rad(deg) {
+    return deg * (Math.PI / 180)
   }
 
 }
