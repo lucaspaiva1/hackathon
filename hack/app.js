73dc6e41a9c86e5936bd6602bb6571309a0ab033
CleybsonCardoso <cleybsonc@gmail.com>
2017-03-17 03:24:06 -0300

    matrix os

--- hack/app.js
+++ hack/app.js
@@ -16,13 +16,13 @@ var users = database.ref('/hackathon');
 
 var temp = {
     arc: 90,
-    color: 'purple',
+    color: 'blue',
     start: 340
 };
 
 var ar = {
     angle: 0,
-    color: 'purple'
+    color: 'blue'
 };
 
 var cafe = {
@@ -38,103 +38,103 @@ var lamp = {
 var porta = {
     arc: 90,
     color: 'red',
-    start:170
+    start: 170
 };
 
-database.ref('/hackathon/-KfOaJ1Tevv_LI99JFc8').update({trancar:false});
+database.ref('/hackathon/-KfOaJ1Tevv_LI99JFc8').update({ trancar: false });
 
 
 
-users.on("value", function(snapshot){
-        var devices = snapshot.val();
-        var objTemp = null;
-        var x = Object.keys(devices).map(function(key){
-            objTemp = devices[key];
-            objTemp.key = key;
-            return objTemp;
-        }).map(function(user){
-            if (user.ar){
-                ar = {
-                    arc: 70,
-                    color: 'rgba(255, 0, 100, 1)',
-                    start: 270
-                };
-            } else{
-                ar = {
-                    arc: 70,
-                    color: 'black',
-                    start:270
-                };
-            }
+users.on("value", function (snapshot) {
+    var devices = snapshot.val();
+    var objTemp = null;
+    var x = Object.keys(devices).map(function (key) {
+        objTemp = devices[key];
+        objTemp.key = key;
+        return objTemp;
+    }).map(function (user) {
+        if (user.ar) {
+            ar = {
+                arc: 70,
+                color: 'blue',
+                start: 270
+            };
+        } else {
+            ar = {
+                arc: 70,
+                color: 'black',
+                start: 270
+            };
+        }
 
-            if (user.cafe){
-                cafe = {
-                    arc: 50,
-                    color: 'rgb(255,50,40)',
-                    start: 70
-                };
-            } else {
-                cafe = {
-                    arc: 50,
-                    color: 'black',
-                    start: 70
-                };
-            }
+        if (user.cafe) {
+            cafe = {
+                arc: 50,
+                color: '#FF9933',
+                start: 70
+            };
+        } else {
+            cafe = {
+                arc: 50,
+                color: 'black',
+                start: 70
+            };
+        }
 
-            if(user.lamp){
-                lamp = {
-                    arc: 50,
-                    color: 'blue',
-                    start: 120
-                };
-            } else {
-                lamp = {
-                    arc: 50,
-                    color: 'black',
-                    start: 120
-                };
-            }
+        if (user.lamp) {
+            lamp = {
+                arc: 50,
+                color: 'white',
+                start: 120
+            };
+        } else {
+            lamp = {
+                arc: 50,
+                color: 'black',
+                start: 120
+            };
+        }
 
-            if (user.trancar){
-                porta = {
+        if (user.trancar) {
+            porta = {
                 arc: 90,
                 color: 'green',
                 start: '170'
-                };
-            }else {
-                porta = {
+            };
+        } else {
+            porta = {
                 arc: 90,
                 color: 'red',
                 start: '170'
-                };
-            }
-
-            var test = user.tempD;
-            test = test - 6;
-            test = test / 2;
-            test = parseInt(test);
-
-            var brilho = 1;
-
-            temp = {
-                arc: 10 * test,
-                color: 'rgba(255, 0, 100, 0.1)',
-                start: 340
             };
+        }
 
-            matrix.led([ar, cafe, lamp, porta, temp]).render();
-        });
-        
+        var test = user.tempD;
+        test = test - 6;
+        test = test / 2;
+        test = parseInt(test);
+
+        var brilho = 1;
+
+        temp = {
+            arc: 10 * test,
+            color: 'blue',
+            start: 340
+        };
+
+        matrix.led([ar, cafe, lamp, porta, temp]).render();
+    });
+
 });
 
-matrix.service('face').start().then(function(data) {
-    
-   
-    database.ref('/hackathon/-KfOaJ1Tevv_LI99JFc8').update({trancar:true});
-    
-    setTimeout(function() {
-      
-        
+matrix.service('face').start().then(function (data) {
+
+
+    database.ref('/hackathon/-KfOaJ1Tevv_LI99JFc8').update({ trancar: true });
+
+    setTimeout(function () {
+
+
     }, 2000);
 });
 
@@ -143,11 +143,21 @@ var options = {
     timeout: 1000 //milliseconds
 };
 
-matrix.init('temperature', options).then(function(data) {
+var x = 0;
+
+matrix.init('temperature', options).then(function (data) {
     //see below for data formats
-    if (parseInt(temp) !== parseInt(data.value)){
-      temp =  parseInt(data.value);
-      console.log("temperatura: " + temp + "ºC");
-      database.ref('/hackathon/-KfOaJ1Tevv_LI99JFc8').update({temp:temp});
+
+
+    try {
+        if (parseInt(x) !== parseInt(data.value)) {
+            x = parseInt(data.value);
+            console.log("temperatura atual: " + x + "ºC");
+            database.ref('/hackathon/-KfOaJ1Tevv_LI99JFc8').update({ temp: x });
+        }
+    } catch (error) {
+
     }
+
+
 });
